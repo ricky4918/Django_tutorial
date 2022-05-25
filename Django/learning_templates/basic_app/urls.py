ffc5b2cbce0ea9058d2fb390f0aa879319e25f1a
ricky 4918 <ricky4918@gmail.com>
2022-05-25 15:36:10 -0700

    commit

--- Django/learning_templates/basic_app/urls.py
+++ Django/learning_templates/basic_app/urls.py
@@ -1,17 +1,10 @@
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 from basic_app import views
 
-
-
-# TEMPLATE TAGGING
-
+# SET THE NAMESPACE!
 app_name = 'basic_app'
 
-
-urlpatterns = [
-    
-    path('relative/', views.relative, name = 'relative'),
-    path('other/', views.other, name = 'other'),
-    
+urlpatterns=[
+    url(r'^relative/$',views.relative,name='relative'),
+    url(r'^other/$',views.other,name='other'),
 ]
--- Django/learning_templates/basic_app/views.py
+++ Django/learning_templates/basic_app/views.py
@@ -1,16 +1,11 @@
-import re
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context_dict = {'text': 'hello world', 'number': 100}
-    return render(request, 'basic_app/index.html', context_dict)
+    return render(request,'basic_app/index.html')
 
 def other(request):
-    
-    return render(request, 'basic_app/other.html')
-
+    return render(request,'basic_app/other.html')
 
 def relative(request):
-    
-    return render(request, 'basic_app/relative_url_templates.html')
+    return render(request,'basic_app/relative_url_templates.html')
