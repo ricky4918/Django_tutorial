4c3bb47c9a21341d15e5cfa30071e4527a45552f
ricky 4918 <ricky4918@gmail.com>
2022-05-25 15:03:11 -0700

    userlogin_website

---
+++ Django/learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,20 @@
+from django import templates
+
+register = templates.Library()
+
+
+@register.filter(name = 'cut')
+def cut(value, arg):
+    
+    """
+    This cuts out all values of "arg" from the string!
+    
+    """
+    
+    return value.replace(arg, '')
+
+
+
+
+
+#register.filter('cut',cut)
