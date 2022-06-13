93cda92de6680eb3a74261d55fb0b57ba8117d9b
ricky4918 <ricky4918@gmail.com>
2022-06-13 09:32:32 -0700

    blog_project

--- DjangoProject/social_clone_project/simplesocial/simplesocial/urls.py
+++ DjangoProject/social_clone_project/simplesocial/simplesocial/urls.py
@@ -1,4 +1,4 @@
-"""myblog URL Configuration
+"""simplesocial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(),
-         name='logout', kwargs={'next_page': '/'}),
 ]
